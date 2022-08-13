b122526066bc684313e2b137bc8c698b0ee95b9c
Ilgar Ismayilzada <ilqarismayilzade@Ilgars-MacBook.local>
2022-08-14 03:52:55 +0400

    Additions to the project

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 //calendar javascript
 $(function () {
-    $("#datepicker").datepicker({
-        dateFormat: "dd M yy",
-        duration: "fast"
+    $(".calendar").datepicker({
+        dateFormat: 'dd M yy',
+        firstDay: 1
+    });
+
+    $(document).on('click', '.date-picker .input', function (e) {
+        var $me = $(this),
+            $parent = $me.parents('.date-picker');
+        $parent.toggleClass('open');
+    });
+
+
+    $(".calendar").on("change", function () {
+        var $me = $(this),
+            $selected = $me.val(),
+            $parent = $me.parents('.date-picker');
+        $parent.find('.result').children('span').html($selected);
     });
 });
+///
 
 
 //open new invoice area
@@ -29,4 +44,37 @@ newInvoiceBtn.addEventListener('click', () => {
             main.classList.remove('open-invoice')
         }
     })
+})
+
+
+
+const selectTerm = document.querySelector('.select-term')
+const termDropdown = selectTerm.querySelector('.options-dropdwon')
+const termArrow = selectTerm.querySelector("svg")
+const termCurrent = selectTerm.querySelector('.current')
+const termOptions = selectTerm.querySelectorAll(".option")
+selectTerm.addEventListener("click", function (e) {
+    termDropdown.classList.toggle('show')
+    termArrow.classList.toggle('rotate-180')
+    for (let i = 0; i < termOptions.length; i++) {
+        termOptions[i].addEventListener("click", function () {
+            termCurrent.innerText = this.innerText
+        })
+
+    }
+    document.addEventListener('click', (e) => {
+        const clickedArea = e.composedPath();
+        if (!clickedArea.includes(selectTerm)) {
+            termDropdown.classList.remove("show")
+            termArrow.classList.remove('rotate-180')
+        }
+    })
+})
+
+
+const datePicker = document.querySelector('.date-picker')
+const dateInput = datePicker.querySelector('.input.result')
+const calendar = datePicker.querySelector('.calendar')
+dateInput.addEventListener('click',function (params) {
+    calendar.classList.toggle('show')
 })
