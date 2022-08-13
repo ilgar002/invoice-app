5348a489cdee9426f5ca142aef461f84a392e053
Ilgar Ismayilzada <ilqarismayilzade@Ilgars-MacBook.local>
2022-08-13 05:27:29 +0400

    Additions to the project

--- app.js
+++ app.js
@@ -11,20 +11,22 @@ $(function () {
 const newInvoiceBtn = document.querySelector('.new-invoice-btn')
 const main = document.querySelector('main')
 const createNewInvoice = document.querySelector('.create-new-invoice')
+
+const header = document.querySelector('header')
+const headerChi = header.querySelectorAll("header *")
+const layout = document.querySelector('.filter-layout')
+
+
 newInvoiceBtn.addEventListener('click', () => {
     main.classList.add('open-invoice')
-    outsideClick(createNewInvoice)
-})
-
-// //outside click close dropdown
-function outsideClick(el) {
-    let clickCounter = 0
     document.addEventListener('click', (e) => {
-        clickCounter++
-        const clickedArea = e.composedPath();
-        if (!clickedArea.includes(el) && clickCounter > 1) {
+        for (let i = 0; i < headerChi.length; i++) {
+            if (headerChi[i] == e.target) {
+                main.classList.remove('open-invoice')
+            }
+        }
+        if (e.target == layout || e.target == header) {
             main.classList.remove('open-invoice')
-            clickCounter = 0
         }
     })
-}
+})
