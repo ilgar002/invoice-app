43a557dd644372ed6bfb9b5dbf0a108f544966e7
Ilgar Ismayilzada <ilqarismayilzade@Ilgars-MacBook.local>
2022-08-15 23:02:35 +0400

    Additions to the project

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ $(function () {
 const newInvoiceBtn = document.querySelector('.new-invoice-btn')
 const main = document.querySelector('main')
 const createNewInvoice = document.querySelector('.create-new-invoice')
-
 const header = document.querySelector('header')
 const headerChi = header.querySelectorAll("header *")
 const layout = document.querySelector('.filter-layout')
@@ -44,8 +43,6 @@ newInvoiceBtn.addEventListener('click', () => {
         }
     })
 })
-
-
 
 const selectTerm = document.querySelector('.select-term')
 const termDropdown = selectTerm.querySelector('.options-dropdwon')
@@ -68,7 +65,6 @@ selectTerm.addEventListener("click", function (e) {
         }
     })
 })
-
 
 const statusFilter = document.querySelector('.filter-status')
 const filterDropdown = statusFilter.querySelector('.options-dropdown')
@@ -102,3 +98,83 @@ dateInput.addEventListener('click', function (params) {
         }
     })
 })
+
+
+const addItem = document.querySelector(".add-item")
+const items = document.querySelector('.items')
+addItem.addEventListener('click', () => {
+    const item = document.createElement('div')
+    item.classList.add('item')
+    item.innerHTML = `<div class="col">
+                            <label class="description">Item Name
+                                <small class="empty-alert">can't be empty</small>
+                            </label>
+                            <input type="text">
+                        </div>
+                        <div class="col">
+                            <label class="description">Qty.
+                                <small class="empty-alert">can't be empty</small>
+                            </label>
+                            <input type="text" value="1">
+                        </div>
+                        <div class="col">
+                            <label class="description">Price
+                                <small class="empty-alert">can't be empty</small>
+                            </label>
+                            <input type="text">
+                        </div>
+                        <div class="col">
+                            <label class="description">Total</label>
+                            <span class="total">0.00</span>
+                        </div>
+                        <button class="delete-item">
+                            <svg width="13" height="16" xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473 0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+                                    fill-rule="nonzero"></path>
+                            </svg>
+                        </button>`
+    items.append(item)
+    deleteItem()
+    itemPrice(item)
+})
+
+//delete item
+function deleteItem() {
+    const deleteItemBtn = items.querySelectorAll('.delete-item')
+    let limit = deleteItemBtn.length
+    for (let i = 0; i < deleteItemBtn.length; i++) {
+        deleteItemBtn[i].addEventListener('click', function () {
+            if (limit > 1) {
+                this.parentElement.remove()
+                limit--
+            }
+        })
+    }
+}
+//count item price
+function itemPrice(item) {
+    if (item) {
+        const inputs = item.querySelectorAll('.item .col input')
+        const total = item.querySelector('.total')
+        countItemTotal(inputs[1], inputs[2], total)
+        
+    }
+    else {
+        const inputs = document.querySelectorAll('.item .col input')
+        const total = document.querySelector('.total')
+        countItemTotal(inputs[1], inputs[2], total)
+    }
+}
+function countItemTotal(qnt,price,total) {
+    qnt.addEventListener('input',()=>{
+        total.innerText=`${qnt.value*price.value}`
+    })
+    price.addEventListener('input', () => {
+        total.innerText = `${qnt.value * price.value}`
+    })
+}
+
+deleteItem()
+itemPrice()
+
